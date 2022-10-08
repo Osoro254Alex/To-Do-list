4f1cd599853a6f7c6872eb860de3d6b51ceb7379
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-10-08 04:31:05 +0300

    Modify src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style.css';
 import loadList from './module/loadList.js';
 import storeArray from './module/storeArray.js';
+import getLocalStore from './module/fromlocalstore.js';
 
 const list = [];
+
+const listCont = document.querySelector('.list-cont');
 const mainInput = document.querySelector('.main-input');
 const bigCont = document.querySelector('.container');
+
 const objCreater = () => {
   const listObj = {};
   listObj.description = mainInput.value;
@@ -22,11 +26,42 @@ bigCont.addEventListener('keypress', (e) => {
       list.push(objects);
       list.sort((a, b) => a.index - b.index);
       storeArray(list);
+      getLocalStore();
       e.preventDefault();
+    }
+    // editing
+    if (e.target.className === 'main-inputs') {
+      const spot = e.target.parentElement.children[2].children[1].id;
+      list.forEach((item) => {
+        if (item.index === Number(spot)) {
+          item.description = e.target.value;
+        }
+      });
+      localStorage.setItem('arraylist', JSON.stringify(list));
     }
   }
 });
 
+const onLoader = () => {
+  if (getLocalStore() !== undefined) {
+    getLocalStore().forEach((elem) => {
+      listCont.innerHTML += `
+        <li class="list-items">
+          <input class="check" type="checkbox">
+          <input class="main-inputs" value = ${elem.description} />
+          <div class="icon">
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <i id="${elem.index}" class="sective fa-solid fa-trash-can"></i>
+          </div>
+        </li>`;
+    });
+  }
+};
+
+onLoader();
+
+const gotted = getLocalStore();
+localStorage.setItem('arraylist', JSON.stringify(gotted));
 bigCont.addEventListener('click', (e) => {
 // toggle on the LineThrough
   if (e.target.className === 'check') {
@@ -56,8 +91,24 @@ bigCont.addEventListener('click', (e) => {
   if (e.target.className === 'fa-solid fa-trash-can') {
     const removePare = e.target.parentElement.parentElement;
     const spot = e.target.id;
-    removePare.remove();
-    list.splice(spot, 1);
-    localStorage.setItem('arraylist', JSON.stringify(list));
+    if (list.length > 0) {
+      list.forEach((item, index) => {
+        if (item.index === Number(spot)) {
+          removePare.remove();
+          list.splice(index, 1);
+        }
+      });
+
+      localStorage.setItem('arraylist', JSON.stringify(list));
+    } else {
+      gotted.forEach((item, index) => {
+        if (item.index === Number(spot)) {
+          removePare.remove();
+          gotted.splice(index, 1);
+        }
+      });
+
+      localStorage.setItem('arraylist', JSON.stringify(gotted));
+    }
   }
 });
