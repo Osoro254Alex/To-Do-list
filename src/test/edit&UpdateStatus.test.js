f20da877a1817a39b8d5d1b0b8d4220043c1e102
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-10-19 16:58:03 +0300

    Implementing all functions of add, remove and Edit

---
+++ src/test/edit&UpdateStatus.test.js
@@ -0,0 +1,8 @@
+import mockElem from '../__mock__/store.js';
+import Item from './addandremove.js';
+
+const statusClear = new Item('item1', 1, false);
+const edit1 = new Item('item11', 0, false);
+const edit2 = new Item('item22', 1, false);
+const statusUpdateFirst = new Item('item3', 0, false);
+const statusUpdateSec = new Item('item4', 2, false);
