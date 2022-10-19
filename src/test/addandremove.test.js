2e4aac08fb760f780da14005464597d778c93148
Abdul Saboor Sohaib <abdulsaboorsohaib@gmail.com>
2022-10-19 14:21:39 +0430

    Add task test.

--- src/test/addandremove.test.js
+++ src/test/addandremove.test.js
@@ -1,2 +1,11 @@
 import mockElem from '../__mock__/store.js';
 import Item from './addandremove.js';
+
+describe('Test for Task Addition', () => {
+  const testItems = new Item('task123', 6, false);
+  const testItems2 = new Item('task23', 2, false);
+  test('add items to list', () => {
+    expect(Item.addTask(testItems.getItem()).length).toBe(mockElem.length);
+    expect(Item.addTask(testItems2.getItem()).length).toBe(mockElem.length);
+  });
+});
