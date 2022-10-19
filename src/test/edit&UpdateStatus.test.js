2aaba1565c46a9bf327b443d52f174d8c0248a4f
Brian Ludidi <bludidi5@gmail.com>
2022-10-19 16:03:31 +0200

    Update edit&UpdateStatus.test.js

--- src/test/edit&UpdateStatus.test.js
+++ src/test/edit&UpdateStatus.test.js
@@ -6,3 +6,9 @@ const edit1 = new Item('item11', 0, false);
 const edit2 = new Item('item22', 1, false);
 const statusUpdateFirst = new Item('item3', 0, false);
 const statusUpdateSec = new Item('item4', 2, false);
+
+describe('Test for Task Edit, Update, and Clear Complete Status', () => {
+  test('Test for Clear All Completed Tasks', () => {
+    expect(statusClear.clearingTask(mockElem).length).toBe(1);
+  });
+});
